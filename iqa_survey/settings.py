0a873457880abfde717a2ce1aa342880bcc4ba64
Natania Deandra <nataniadeandra@gmail.com>
2025-06-07 22:12:22 +0700

    feat: added summary of images on final page

--- iqa_survey/settings.py
+++ iqa_survey/settings.py
@@ -75,17 +75,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'iqa_survey.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': os.getenv('DB_HOST'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PORT': os.getenv('DB_PORT'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv('DB_HOST'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PORT': os.getenv('DB_PORT'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
